40ff4318b88248a88fdd37b315e2273ecd60d1b1
jwaynick99 <jwaynick99@gmail.com>
2024-11-04 20:25:21 -0600

    Task one completed, unsure of sql comment

--- queries.sql
+++ queries.sql
@@ -1,5 +1,11 @@
 --Part 1
-
+/*
+    id int,
+    employer VARCHAR(255),
+    name VARCHAR(255),
+    skills VARCHAR(255)
+    PRIMARY KEY(id)
+*/
 --Part 2
 
 --Part 3
--- src/main/java/org/launchcode/techjobs/persistent/models/AbstractEntity.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/AbstractEntity.java
@@ -8,11 +8,15 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 import java.util.Objects;
-
+@MappedSuperclass
 public abstract class AbstractEntity {
 
+    @Id
+    @GeneratedValue
     private int id;
 
+    @NotNull(message = "Name is required")
+    @Size(min = 2, max = 100, message = "Name must be between 2 and 100 characters")
     private String name;
 
     public int getId() {
